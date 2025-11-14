087289c7425d30ca047a429ba95ed30a859095f0
Студент группы IS <is@sielom.local>
2025-11-14 18:02:57 +0500

    first

--- Projectprogrammingcourses/courseapp/admin.py
+++ Projectprogrammingcourses/courseapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Teachers)
+admin.site.register(Group)
+admin.site.register(Practice)
+admin.site.register(Students)
+admin.site.register(Journal)
+admin.site.register(Courses)
--- Projectprogrammingcourses/courseapp/models.py
+++ Projectprogrammingcourses/courseapp/models.py
@@ -1,3 +1,84 @@
 from django.db import models
 
-# Create your models here.
+class Teachers(models.Model):
+    name = models.CharField("Имя", max_length=50, default="")
+    surname = models.CharField("Фамилия", max_length=50, default="")
+    surname2 = models.CharField("Отчество", max_length=50, default="")
+    birthday = models.DateField("Дата рождения")
+    gender = models.CharField("Пол")
+    phone_number = models.CharField("Номер телефона", max_length=11)
+    
+    def __str__(self):
+        return f"{self.surname}"
+    class Meta:
+        verbose_name = "Преподаватель"
+        verbose_name_plural = "Преподаватели"
+        ordering = ["surname","name", "surname2"]
+       # index = [
+       #     models.Index(fields=["surname"])
+       #]
+        
+        #constraints = [
+        #   models.UniqueConstraint(
+        #       fields=["surname", "name"],
+        #       condition=models.Q(tip="")
+        #   )
+        #]
+        
+    
+class Group(models.Model):
+    name = models.CharField("Название", max_length=50, unique=True)
+    teacher_id = models.ForeignKey(Teachers, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.name}"
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
+    
+class Practice(models.Model):
+    name = models.CharField("Название", max_length=100, unique=True)
+    date = models.DateField("Дата")
+    assessment = models.IntegerField("Оценка")
+    teacher_id = models.ForeignKey(Teachers, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.name}"
+    class Meta:
+        verbose_name = "Учебная Практика"
+        verbose_name_plural = "Учебная Практика"
+    
+class Students(models.Model):
+    name = models.CharField("Имя", max_length=50, default="")
+    surname = models.CharField("Фамилия", max_length=50, default="")
+    surname2 = models.CharField("Отчество", max_length=50, default="")
+    birthday = models.DateField("Дата рождения")
+    gender = models.CharField("Пол")
+    phone_number = models.CharField("Номер телефона", max_length=11)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    practice_id = models.ManyToManyField(Practice)
+    
+    def __str__(self):
+        return f"{self.surname}"
+    class Meta:
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенты"
+        ordering = ["surname","name", "surname2"]
+    
+class Journal(models.Model):
+    group_id = models.ForeignKey(Group,on_delete=models.CASCADE)
+    students_id = models.ForeignKey(Group, related_name='+', on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Group, related_name='+',on_delete=models.CASCADE)
+    date = models.DateField("Дата")
+    assessment = models.IntegerField("Оценка")
+    def __str__(self):
+        return f"{self.date}"
+    class Meta:
+        verbose_name = "Журнал"
+        verbose_name_plural = "Журнал"
+    
+class Courses(models.Model):
+    name = models.CharField("Название", max_length=100, unique=True)
+    group_id = models.ManyToManyField(Group)
+    teachers_id = models.ManyToManyField(Teachers)
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
